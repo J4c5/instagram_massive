ee37679efcbc2e1d1430b931d81443c43194a801
ninja <88632118+gh-ninja@users.noreply.github.com>
2021-10-21 19:23:47 -0300

    Create follower bot.py

---
+++ example/follower bot.py
@@ -0,0 +1,16 @@
+# Logic - creating a simple follower bot with it
+# code:
+from src.instagram import instagram; insta = instagram()
+from instagram_private_api import Client, ClientCompatPatch
+
+Account = insta.NewAccount()
+user_name = Account.Email
+password = Account.Pass
+
+print(f"Bot - Using: {user_name}, {password}")
+api = Client(user_name, password)
+instaId = "11873066713"
+
+print("Follow {instaId}")
+api.friendships_create(instaId) # follow this
+
